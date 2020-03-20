fcec4886115581cf83e858b6617c2e7929d4d879
lipuisaboy <1213262105@qq.com>
2020-03-20 17:18:56 +0800

    改变上传接口

--- server/routes/index.js
+++ server/routes/index.js
@@ -51,7 +51,7 @@ module.exports = app => {
   const upload = multer({dest:__dirname + '/../uploads'})
   app.post('/wzry/api/upload',upload.single('file') ,async(req,res) => {
     const file =await req.file
-    file.url = `http://123.207.196.89:3002/uploads/${file.filename}`
+    file.url = `http://47.107.69.83:3002/uploads/${file.filename}`
     res.send(file)
   })
 
